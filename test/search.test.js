ad65e64939d88435f85dcae8fc5d9f4768430eb8
Daniel Moniz <daniel.moniz@groupbyinc.com>
2019-05-31 11:12:05 -0400

    WIP: Add one failing test.

---
+++ test/search.test.js
@@ -0,0 +1,11 @@
+const search = require('../src/plugins/data-sources/search')
+const chai = require('chai')
+const expect = chai.expect
+
+describe('search module', () => {
+  describe('search', () => {
+    it('should fail', () => {
+      expect(true).to.equal(false)
+    })
+  })
+})
