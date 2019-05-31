f9445f36ee1002ffaf82e0345feafd877099ac59
Daniel Moniz <daniel.moniz@groupbyinc.com>
2019-05-31 11:16:58 -0400

    Test that the search module has a name.

--- test/search.test.js
+++ test/search.test.js
@@ -1,11 +1,17 @@
-const search = require('../src/plugins/data-sources/search')
+const core = require('../src/core')
+const getSearch = require('../src/plugins/data-sources/search').default
 const chai = require('chai')
 const expect = chai.expect
 
 describe('search module', () => {
+  let search
+  beforeEach(() => {
+    search = getSearch(core)
+  })
+
   describe('search', () => {
-    it('should fail', () => {
-      expect(true).to.equal(false)
+    it('should have a name', () => {
+      expect(search.name).to.equal('search')
     })
   })
 })
