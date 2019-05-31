5f84ed2c1d3758a868f002ad929bfb39e738996e
Daniel Moniz <daniel.moniz@groupbyinc.com>
2019-05-31 16:35:16 -0400

    Add missing events-browser file.

---
+++ src/plugins/events-browser.js
@@ -0,0 +1,16 @@
+// Should have the ability to listen to and fire events.
+
+// addEventListener('gb-plugin', (event) => {
+//   console.log(event.type)
+//   console.log(event.detail)
+// })
+
+export default function() {
+  return {
+    name: 'events',
+    description: 'Allows other plugins to fire and listen for events.',
+    listen: function(type, callback) {
+      return addEventListener(type, callback)
+    },
+  }
+}
