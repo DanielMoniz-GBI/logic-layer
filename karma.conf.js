e1b5fc5fcff20954e3658d72796f70a14fe2843e
Daniel Moniz <daniel.moniz@groupbyinc.com>
2019-06-04 11:19:13 -0400

    WIP: get one test passing and one failing.

--- karma.conf.js
+++ karma.conf.js
@@ -1,22 +1,28 @@
-module.exports = function (config) {
-    config.set({
-        frameworks: ['mocha', 'chai'],
-        files: [
-            'dist/**/index-browser.js',
-            'test/**/*.test.browser.js'
-        ],
-        reporters: ['progress'],
-        port: 9876,  // karma web server port
-        colors: true,
-        logLevel: config.LOG_INFO,
-        browsers: ['ChromeHeadless', 'Firefox', 'FirefoxDeveloper', 'FirefoxNightly', 'IE'],
-        autoWatch: false,
-        concurrency: Infinity,
-        customLaunchers: {
-            FirefoxHeadless: {
-                base: 'Firefox',
-                flags: ['-headless'],
-            },
-        },
-    })
-}
+/* eslint-disable import/no-extraneous-dependencies */
+const createDefaultConfig = require('@open-wc/testing-karma/default-config');
+const merge = require('webpack-merge');
+
+module.exports = config => {
+  config.set(
+    merge(createDefaultConfig(config), {
+      basePath: '',
+      frameworks: ['mocha', 'chai'],
+      files: [
+        // runs all files ending with .test in the test folder,
+        // can be overwritten by passing a --grep flag. examples:
+        //
+        // 'node_modules/chai/chai.js',
+        // npm run test -- --grep test/foo/bar.test.js
+        // npm run test -- --grep test/bar/*
+        // { pattern: config.grep ? config.grep : 'test/**/*.test.browser.js', type: 'module' },
+        // { pattern: 'node_modules/chai/chai.js', watched: true, included: false, served: true, type: 'module' },
+        // { pattern: 'node_modules/axios/dist/axios.js', watched: true, included: false, served: true, type: 'module' },
+        // { pattern: 'src/**/*.js', watched: true, included: true, served: true, type: 'module' },
+        { pattern: 'test/**/*.test.browser.js', type: 'module' },
+      ],
+
+      // you can overwrite/extend the config further
+    }),
+  );
+  return config;
+};
