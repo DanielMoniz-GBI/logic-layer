63ed8ea57f9151d7c8a0d3f10908fe22727e8be4
Daniel Moniz <daniel.moniz@groupbyinc.com>
2019-06-04 11:28:51 -0400

    Uncomment browser tests and get them passing.

--- karma.conf.js
+++ karma.conf.js
@@ -11,13 +11,9 @@ module.exports = config => {
         // runs all files ending with .test in the test folder,
         // can be overwritten by passing a --grep flag. examples:
         //
-        // 'node_modules/chai/chai.js',
         // npm run test -- --grep test/foo/bar.test.js
         // npm run test -- --grep test/bar/*
         // { pattern: config.grep ? config.grep : 'test/**/*.test.browser.js', type: 'module' },
-        // { pattern: 'node_modules/chai/chai.js', watched: true, included: false, served: true, type: 'module' },
-        // { pattern: 'node_modules/axios/dist/axios.js', watched: true, included: false, served: true, type: 'module' },
-        // { pattern: 'src/**/*.js', watched: true, included: true, served: true, type: 'module' },
         { pattern: 'test/**/*.test.browser.js', type: 'module' },
       ],
 
--- test/browser/events.test.browser.js
+++ test/browser/events.test.browser.js
@@ -1,54 +1,41 @@
-// import {expect} from '/base/node_modules/chai/lib/chai.js'
-// import chai from 'chai'
-// import getEvents from '/base/src/plugins/events-browser'
-// const getEvents = require('../src/plugins/events-node').default
+import getEvents from '/base/src/plugins/events-browser.js'
 
-describe('sample tests', () => {
-  it('should pass', () => {
-    expect(true).to.be.true
+describe('events', () => {
+  let events
+  beforeEach(() => {
+    events = getEvents()
   })
 
-  it('should fail', () => {
-    expect(true).to.be.true
+  describe('contract', () => {
+    it('should have a name', () => {
+      expect(events.name).to.equal('events')
+    })
+
+    it('should have a description', () => {
+      expect(typeof events.description).to.be.a('string')
+      expect(events.description.length).to.be.greaterThan(0)
+    })
+  })
+
+  describe('listen', () => {
+    it('should be a function', () => {
+      expect(events.listen).to.be.a('function')
+    })
+
+    it('should call the provided function after the relevant event type', () => {
+      let eventCalled = false
+      events.listen('test-event', () => eventCalled = true)
+      events.dispatch('test-event')
+      expect(eventCalled).to.be.true
+    })
+
+    it('should receive an event object with a type and a detail payload', () => {
+      const payload = { key1: 'val1', key2: 'val2' }
+      events.listen('test-event', (event) => {
+        expect(event.type).to.equal('test-event')
+        expect(event.detail).to.deep.equal(payload)
+      })
+      events.dispatch('test-event', payload)
+    })
   })
 })
-
-// describe('events', () => {
-//   let events
-//   beforeEach(() => {
-//     events = getEvents()
-//   })
-//
-//   describe('contract', () => {
-//     it('should have a name', () => {
-//       expect(events.name).to.equal('events')
-//     })
-//
-//     it('should have a description', () => {
-//       expect(typeof events.description).to.be.a('string')
-//       expect(events.description.length).to.be.greaterThan(0)
-//     })
-//   })
-//
-//   describe('listen', () => {
-//     it('should be a function', () => {
-//       expect(events.listen).to.be.a('function')
-//     })
-//
-//     it('should call the provided function after the relevant event type', () => {
-//       let eventCalled = false
-//       events.listen('test-event', () => eventCalled = true)
-//       events.dispatch('test-event')
-//       expect(eventCalled).to.be.true
-//     })
-//
-//     it('should receive an event object with a type and a detail payload', () => {
-//       const payload = { key1: 'val1', key2: 'val2' }
-//       events.listen('test-event', (event) => {
-//         expect(event.type).to.equal('test-event')
-//         expect(event.detail).to.deep.equal(payload)
-//       })
-//       events.dispatch('test-event', payload)
-//     })
-//   })
-// })
